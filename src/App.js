5964d5e4cddd67dcc41427d39cebf8d25d707954
Rupesh Malpani <Rupesh.Malpani@skillsoft.com>
2020-10-18 17:29:11 +0530

    simple react calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import KeypadComponent from './components/KeypadComponents';
+import OutputComponent from './components/OutputComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: ""
+    }
+  }
+
+  onClick = button => {
+    if (button === "=") {
+      this.onCalculate()
+    }
+    else if (button === "C") {
+      this.onReset()
+    }
+    else if (button === "CE") {
+      this.onBackspace()
+    }
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  }
+
+  onCalculate = () => {
+    // var checkResult = ''
+    // if (this.state.result.includes('--')) {
+    //   checkResult = this.state.result.replace('--', '+')
+    // }
+
+    // else {
+    //   checkResult = this.state.result
+    // }
+    try {
+      // eslint-disable-next-line
+      this.setState({
+        // eslint-disable-next-line
+        result: (eval(this.state.result) || "") + ""
+      })
+    } catch (error) {
+      this.setState({
+        result: "error"
+      })
+    }
+
+  }
+
+  onReset = () => {
+    this.setState({
+      result: ""
+    })
+  }
+
+  onBackspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <h1 style={{ textAlign: 'center' }}>Simple Calculator</h1>
+        <div className="calculatorContainer">
+          <OutputComponent result={this.state.result} />
+          <KeypadComponent onClick={this.onClick} />
+        </div>
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
---
+++ src/components/OutputComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OutputComponent = ({ result }) => {
+    return (
+        <div className="outputContainer">{result}</div>
+    )
+}
+
+export default OutputComponent
